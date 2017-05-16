9b1ed24280f2a00b3ad564fb3eec0a724e31cc8c
盧瑞山 <admin@luruishande-MacBook-Pro.local>
2017-05-16 16:08:03 +0800

    modified the source code from Lursun

--- btcaccounttrack_lursun.py
+++ btcaccounttrack_lursun.py
@@ -2,6 +2,7 @@
 import requests
 import time
 print 'Taiwan local time:', time.ctime()
+print ''
 from bs4 import BeautifulSoup
 res1 = requests.get("https://blockchain.info/address/115p7UMMngoj1pMvkpHijcRdfJNXj6LrLn")
 res2= requests.get("https://blockchain.info/address/12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw")
@@ -17,24 +18,27 @@ for n3 in soup3.select("#total_received"):
      print 'WannaCry Account_03 = ',n3.text
 
 for t1 in soup1.select("#n_transactions"):
-     print 'Number Transactions01 = ',n1.text
+     print 'Number Transactions in account_01 = ',t1.text,'txs'
 for t2 in soup2.select("#n_transactions"):
-     print 'Number Transactions02 = ',n2.text
+     print 'Number Transactions in account_02 = ',t2.text,'txs'
 for t3 in soup3.select("#n_transactions"):
-     print 'Number Transactions03 = ',n3.text
+     print 'Number Transactions in account_03 = ',t3.text,'txs'
 
 total = float(n1.text.replace(' BTC','')) + float(n2.text.replace(' BTC','')) + float(n3.text.replace(' BTC',''))
 ts=int(t1.text)+int(t2.text)+int(t3.text)
 print 'WannaCry total received =', total, 'BTC'
 print 'WannaCry total received =', total*1750, 'USD' , '(in the rate 1BTC=1750 USD)'
 print 'WannaCry total received =', total*1750*30.2, 'NT' , '(in the rate 1USD=30.2 TWD)'
-print 'WannaCry average received =', total/ts, 'BTC'
-print 'WannaCry average received =', (total/ts)*1750, 'USD'
-print 'WannaCry average received =', (total/ts)*1750*30.2, 'NT'
-
+print 'WannaCry total transcations received=',ts,'txs'
+print 'WannaCry average received per transaction =', total/ts, 'BTC'
+print 'WannaCry average received per transaction =', (total/ts)*1750, 'USD'
+print 'WannaCry average received per transaction =', (total/ts)*1750*30.2, 'NT'
+print ''
 print '勒索軟體WannaCry總共收到贖款共 =', total, '比特幣'
 print '勒索軟體WannaCry總共收到贖款共：', total*1750, '美元' , '(以1比特幣=1750美元換算)'
 print '勒索軟體WannaCry總共收到贖款共：', total*1750*30.2, '新台幣' , '(以1美元=30.2新台幣換算)'
-print '勒索軟體WannaCry平均贖款 =', total/ts, '比特幣'
-print '勒索軟體WannaCry平均贖款 =', (total/ts)*1750, '美元'
-print '勒索軟體WannaCry平均贖款 =', (total/ts)*1750*30.2, '新台幣'
+print '勒索軟體WannaCry總共收到贖款筆數:',ts,'筆'
+print '勒索軟體WannaCry平均每筆贖款 =', total/ts, '比特幣'
+print '勒索軟體WannaCry平均每筆贖款 =', (total/ts)*1750, '美元'
+print '勒索軟體WannaCry平均每筆贖款 =', (total/ts)*1750*30.2, '新台幣'
+
